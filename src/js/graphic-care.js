54786ec8dc83304d6aa1387e2b0fbfdac61de7e4
russellgoldenberg <russellgoldenberg@gmail.com>
2018-08-02 16:06:33 -0400

    fix bug

--- src/js/graphic-care.js
+++ src/js/graphic-care.js
@@ -14,7 +14,11 @@ let $tip = null;
 
 function filter({ name, value }) {
 	const $person = $ul.selectAll('.person');
-	if (name) $person.classed('is-faded', d => !d[name].includes(value));
+	if (name)
+		$person.classed('is-faded', d => {
+			console.log(d.display, d[name]);
+			return !d[name].includes(value);
+		});
 	else $person.classed('is-faded', false);
 }
 
